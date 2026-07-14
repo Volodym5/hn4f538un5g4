c7bccec345a7bb67a9a237f09e7a13d51f31d228
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2026-07-14 20:49:27 +0300

    Update BunnyHop.lua

--- src/features/movement/BunnyHop.lua
+++ src/features/movement/BunnyHop.lua
@@ -3,22 +3,30 @@ BunnyHop.__index = BunnyHop
 
 function BunnyHop.new(context)
     local self = setmetatable({}, BunnyHop)
-
     self.services = context.services
     self.globals = context.globals
     self.cleaner = context.Cleaner.new()
     self.errorHandler = context.errorHandler
     self.enabled = false
+    local Modules = {}
+    for Index, Value in getloadedmodules() do
+        local Ok, Module = pcall(require, Value)
+        if Ok and typeof(Module) == "table" and Module then
+            if Module.jump then
+                Modules["Controllers/CharacterController"] = Module
+                continue
+            end
+        end
+    end
+    self.modules = Modules
 
     self.cleaner:Give(self.errorHandler:Connect(self.services.RunService.RenderStepped, "BunnyHop RenderStepped", function()
         if not self.enabled then
             return
         end
-
         if not self.globals:IsAlive() then
             return
         end
-
         if not self.services.UserInputService:IsKeyDown(Enum.KeyCode.Space) then
             return
         end
@@ -29,9 +37,14 @@ function BunnyHop.new(context)
             return
         end
 
+        local characterController = self.modules["Controllers/CharacterController"]
+        if not characterController then
+            return
+        end
+
         local state = humanoid:GetState()
         if state ~= Enum.HumanoidStateType.Jumping and state ~= Enum.HumanoidStateType.Freefall then
-            humanoid.Jump = true
+            characterController.jump()
         end
     end))
 
