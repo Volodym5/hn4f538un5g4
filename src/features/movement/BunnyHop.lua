e9427faf71d2897a5697fa724a54cb3a573869b9
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2026-07-14 21:01:19 +0300

    Update BunnyHop.lua

--- src/features/movement/BunnyHop.lua
+++ src/features/movement/BunnyHop.lua
@@ -1,44 +1,24 @@
-if not LPH_OBFUSCATED then
-    LPH_NO_VIRTUALIZE = function(...)
-		return ...;
-	end;
-end;
-
-local Modules = LPH_NO_VIRTUALIZE(function()
-    local Modules = {}
-
-    for Index, Value in getloadedmodules() do
-        local Ok, Module = pcall(require, Value)
-        if Ok and typeof(Module) == "table" and Module then
-            if Module.jump then
-                Modules["Controllers/CharacterController"] = Module
-                continue
-            end
-        end
-    end
-
-    return Modules
-end)()
-
 local BunnyHop = {}
 BunnyHop.__index = BunnyHop
 
 function BunnyHop.new(context)
     local self = setmetatable({}, BunnyHop)
+
     self.services = context.services
     self.globals = context.globals
     self.cleaner = context.Cleaner.new()
     self.errorHandler = context.errorHandler
     self.enabled = false
-    self.modules = Modules
 
     self.cleaner:Give(self.errorHandler:Connect(self.services.RunService.RenderStepped, "BunnyHop RenderStepped", function()
         if not self.enabled then
             return
         end
+
         if not self.globals:IsAlive() then
             return
         end
+
         if not self.services.UserInputService:IsKeyDown(Enum.KeyCode.Space) then
             return
         end
@@ -49,14 +29,9 @@ function BunnyHop.new(context)
             return
         end
 
-        local characterController = self.modules["Controllers/CharacterController"]
-        if not characterController then
-            return
-        end
-
         local state = humanoid:GetState()
         if state ~= Enum.HumanoidStateType.Jumping and state ~= Enum.HumanoidStateType.Freefall then
-            characterController.jump()
+            humanoid.Jump = true
         end
     end))
 
